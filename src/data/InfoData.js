a46e611937f088da057fb60039d6e541a5abd4b2
Juan Apolinar <juanapolinar@my.unt.edu>
2021-07-07 13:53:45 -0500

    first push

---
+++ src/data/InfoData.js
@@ -0,0 +1,22 @@
+import ImageOne from '../images/interior-1.jpg';
+import ImageTwo from '../images/house-5.jpg';
+
+export const InfoData = {
+    heading: "Explore our Beautiful Homes",
+    paragraphOne: 'sadfa dsfas dfa sdfa sd a sdfa as dfasdfas dfasdfa sdf fasd fasda sdfas dfasdfaa sdf asd asdfas dfasdf.',
+    paragraphTwo: 'asdf asdfa sdfasdf htwe rsgergsdfg sdfgaer gsdfg erg sdfg asdfggsher gadsf g dfsg ers gsd fgsdf gesr gsdf gsdf g.',
+    ButtonLabel: "View Homes",
+    image: ImageOne,
+    reverse: false,
+    delay: 100
+}
+
+export const InfoDataTwo = {
+    heading: "Modern Designs",
+    paragraphOne: 'sadfa dsfas dfa sdfa sd a sdfa as dfasdfas dfasdfa sdf fasd fasda sdfas dfasdfaa sdf asd asdfas dfasdf.',
+    paragraphTwo: 'asdf asdfa sdfasdf htwe rsgergsdfg sdfgaer gsdfg erg sdfg asdfggsher gadsf g dfsg ers gsd fgsdf gesr gsdf gsdf g.',
+    ButtonLabel: "View Homes",
+    image: ImageTwo,
+    reverse: false,
+    delay: 100
+}
